a2912e9faee7a1bd607c87427f29e97ba03aea16
Valentino Torres <danieltorresdev@gmail.com>
2021-07-21 08:43:07 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Button } from '@valentinotorresdev/test-bit.button'
 
 function App() {
   return (
@@ -9,17 +10,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <Button text="Holi" />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
